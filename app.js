a733e0028ae513732f69771e6e641f6b3c48f592
blvckcoffee <joe.johnson.dev@gmail.com>
2021-01-28 13:34:31 -0600

    completed basic javascript functions exercises

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n')
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n')
+
+function printOdds(count) {
+  if (count == 0 || isNaN(count)) {
+    console.log(`Please enter a positive integer to count to.`)
+  }
+
+  if (count < 0) {
+    count *= -1
+  }
+
+  for (let i = 1; i <= count; i++) {
+    if (i % 2 != 0) {
+      console.log(i)
+    }
+  }
+}
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log('EXERCISE 2:\n==========\n')
+
+function checkAge(name = 'Joe', age = 31) {
+  let aboveSixteen = `You can drive, ${name}!`
+  let belowSixteen = `Sorry, ${name}, but you need to wait until you're 16.`
+
+  if (age >= 16) {
+    console.log(aboveSixteen)
+  } else {
+    console.log(belowSixteen)
+  }
+}
